0dbf7f5931c971264266791bbc7b79a97b350ae9
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-10-22 23:09:40 -0600

    estilos a la carta

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+import '../css/app.css'
+
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/twitter/kikobeats" alt="avatar" />
+        <div>
+          <strong>Roberto</strong>
+          <span>@rober.salama</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  )
+}
+
+export default App
